d8be7b80fc7b7d28a677fff63754d5e6c2d6a931
Gosia <malgorzata.cieslewicz@bewatec.com>
2022-03-28 13:07:33 +0200

    initial commit

---
+++ src/com/behavioral/mediator/UIControl.java
@@ -0,0 +1,4 @@
+package com.behavioral.mediator;
+
+public class UIControl {
+}
---
+++ src/com/behavioral/state/TravelMode.java
@@ -0,0 +1,8 @@
+package com.behavioral.state;
+
+public enum TravelMode {
+    DRIVING,
+    BICYCLING,
+    TRANSIT,
+    WALKING
+}
